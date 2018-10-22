120099de559604b5c1d9fc6e59c21402439e52b5
one <1161372240@qq.com>
2018-10-22 16:07:56 +0800

    Debug 1.2

--- spider.py
+++ spider.py
@@ -1,33 +1,34 @@
 # coding: utf-8
 
-from pyquery import PyQuery as pq
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import requests
-import pymysql
-import time
 import re
 import os
+import time
+import pymysql
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from pyquery import PyQuery as pq
 
 class Spider:
-    def __init__(self, wechat_id):
+    def __init__(self, wechat_ids):
         '''
         构造函数，根据公众号微信号获取对应文章的发布时间、文章标题, 文章链接等信息
         :param
         '''
-        self.wechat_id = wechat_id
+        self.wechat_ids = wechat_ids
 
         # 请求头
         self.headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'}
-
-        # 搜索url
-        self.search_url = 'https://weixin.sogou.com/weixin?type=1&s_from=input&query='+wechat_id
 
         # 超时时长
         self.timeout = 5
 
         # 爬虫模拟在一个request.session中完成
         self.session = requests.Session()
+
+        # 连接数据库
+        self.db = pymysql.connect(host='localhost', user='root', password='12345678', port=3306, db='spider')
+        self.cursor = self.db.cursor()
 
 
     def log(self,msg):
@@ -38,124 +39,130 @@ class Spider:
         '''
         print(u'%s: %s' % (time.strftime('%Y-%m-%d %H:%M:%S'), msg))
 
-    def get_url(self):
+    def get_infos(self):
 
-        self.log(u'公众号为：%s' % self.wechat_id)
-
-        search_html = self.session.get(self.search_url,headers=self.headers, timeout=self.timeout).content
-
-        #获取公众号URL
-        doc = pq(search_html)
-        wechat_url = doc('div[class=txt-box]')('p[class=tit]')('a').attr('href')
-        self.log(u'公众号url：%s' % wechat_url)
-
-        # 获取html
-        browser = webdriver.Chrome()
-        browser.get(wechat_url)
-        time.sleep(2)
-        wechat_html = browser.execute_script("return document.documentElement.outerHTML")
-        browser.close()
-
-        # 检测是否被限制访问
-        if pq(wechat_html)('#verify_change').text() != '':
-            self.log(u'已限制访问，请稍后再试')
-
-        else:
-            # 获取发布时间，标题，首图，URL
-            doc = pq(wechat_html)
-            articles_list = doc('div[class="weui_media_box appmsg"]')
-            articlesLength = len(articles_list)
-            self.log(u'抓到文章%s篇' % articlesLength)
-
-            if articles_list:
-                index = 0
-
-                for article in articles_list.items():
-
-                    self.log('')
-                    self.log('正在爬取(%s/%s)' % (index, articlesLength))
-                    index += 1
-
-                    # 获取标题
-                    title = article('h4[class="weui_media_title"]').text().strip()
-                    self.log(u'标题： %s' % title)
-
-                    # 创建标题前五个字命名的文件夹
-                    # if not os.path.exists(title[:10]):
-                    #     os.makedirs(title[:10])
-                    # path = os.getcwd()+ '/' + title[:10] +'/'
-                    # print(path)
+        for wechat_id in self.wechat_ids:
 
 
-                    # 获取文章发表时间
-                    # date = article('.weui_media_extra_info').text().strip()
-                    # date = article('p[class="weui_media_extra_info"]').text().strip()
-                    temp_date = article('p[class="weui_media_extra_info"]').text().strip()
-                    if temp_date.endswith("原创"):
-                        date = temp_date.replace('原创','')
-                    else:
-                        date = temp_date
-                    self.log(u'发表时间： %s' % date)
+            self.log(u'公众号为：%s' % wechat_id)
 
-                    # 获取标题对应的地址
-                    article_url = 'http://mp.weixin.qq.com' + article('h4[class="weui_media_title"]').attr('hrefs')
-                    self.log(u'地址： %s' % article_url)
+            # 搜索url
+            search_url = 'https://weixin.sogou.com/weixin?type=1&s_from=input&query=' + wechat_id
+            search_html = self.session.get(search_url,headers=self.headers, timeout=self.timeout).content
 
-                    # 获取封面图片
-                    cover = article('.weui_media_hd').attr('style')
+            #获取公众号URL
+            doc = pq(search_html)
+            wechat_url = doc('div[class=txt-box]')('p[class=tit]')('a').attr('href')
+            self.log(u'公众号url：%s' % wechat_url)
 
-                    pic = re.compile(r'background-image:url(.+)')
-                    rs = pic.findall(cover)
-                    if len(rs) > 0:
-                        pic = rs[0].replace('(', '')
-                        pic = pic.replace(')', '')
-                        #self.log(u'封面图片：%s ' % pic)
+            # 获取html
+            browser = webdriver.Chrome()
+            browser.get(wechat_url)
+            time.sleep(2)
+            wechat_html = browser.execute_script("return document.documentElement.outerHTML")
+            browser.close()
 
-                    # 获取正文内容
-                    if title == "分享图片":     # 判断文章是否是为分享图片的类型
-                        content = "shareImg"
-                    else:
-                        content = self.get_atticle_info(article_url)
+            # 检测是否被限制访问
+            if pq(wechat_html)('#verify_change').text() != '':
+                self.log(u'已限制访问，请稍后再试')
 
-                    #print(content)
+            else:
+                # 获取发布时间，标题，首图，URL
+                doc = pq(wechat_html)
+                articles_list = doc('div[class="weui_media_box appmsg"]')
+                articlesLength = len(articles_list)
+                self.log(u'抓到文章%s篇' % articlesLength)
 
-                    # 获取文章图片
-                    imgs = self.get_article_img(article_url)
-                    for img in imgs:
-                        if img.endswith('gif'):
-                            img_url = img + '.gif'
-                        if img.endswith('jpg'):
-                            img_url = img + '.jpg'
-                        if img.endswith('jpeg'):
-                            img_url = img + '.jpeg'
-                        if img.endswith('png'):
-                            img_url = img + '.png'
+                if articles_list:
+                    index = 0
 
-                        # 下载图片到本地
-                        # data = requests.get(img,headers=self.headers)
-                        # fp = open(path+img_url[-15:],'wb')
-                        # fp.write(data.content)
-                        # fp.close()
-                    # print(imgs)
+                    for article in articles_list.items():
 
-                    # 获取html代码
-                    html = requests.get(article_url)
-                    html.encoding = 'utf-8'
+                        self.log('')
+                        self.log('正在爬取(%s/%s)' % (index, articlesLength))
+                        index += 1
 
-                    # 保存数据到数据库
-                    sql = 'INSERT INTO wechat_article(publish_date,article_title,wechat_id,article_url,cover_img,article_content,article_img,article_html) values(%s, %s, %s, %s, %s, %s, %s, %s)'
-                    # 推文为分享其他文章则不入库
-                    if content != 'shareArticle':
-                        try:
-                            cursor.execute(sql,(date, title, self.wechat_id, article_url, pic, content, imgs[0], html.text))
-                            db.commit()
-                            self.log(u'入库成功')
-                        except:
-                            db.rollback()
-                            self.log(u'入库不成功')
+                        # 获取标题
+                        title = article('h4[class="weui_media_title"]').text().strip()
+                        self.log(u'标题： %s' % title)
 
-                    #time.sleep(1)
+                        # 创建标题前五个字命名的文件夹
+                        # if not os.path.exists(title[:10]):
+                        #     os.makedirs(title[:10])
+                        # path = os.getcwd()+ '/' + title[:10] +'/'
+                        # print(path)
 
+
+                        # 获取文章发表时间
+                        # date = article('.weui_media_extra_info').text().strip()
+                        # date = article('p[class="weui_media_extra_info"]').text().strip()
+                        temp_date = article('p[class="weui_media_extra_info"]').text().strip()
+                        if temp_date.endswith("原创"):
+                            date = temp_date.replace('原创','')
+                        else:
+                            date = temp_date
+                        self.log(u'发表时间： %s' % date)
+
+                        # 获取标题对应的地址
+                        article_url = 'http://mp.weixin.qq.com' + article('h4[class="weui_media_title"]').attr('hrefs')
+                        self.log(u'地址： %s' % article_url)
+
+                        # 获取封面图片
+                        cover = article('.weui_media_hd').attr('style')
+
+                        pic = re.compile(r'background-image:url(.+)')
+                        rs = pic.findall(cover)
+                        if len(rs) > 0:
+                            pic = rs[0].replace('(', '')
+                            pic = pic.replace(')', '')
+                            #self.log(u'封面图片：%s ' % pic)
+
+                        # 获取正文内容
+                        if title == "分享图片":     # 判断文章是否是为分享图片的类型
+                            content = "shareImg"
+                        else:
+                            content = self.get_atticle_info(article_url)
+
+                        #print(content)
+
+                        # 获取文章图片
+                        imgs = self.get_article_img(article_url)
+                        for img in imgs:
+                            if img.endswith('gif'):
+                                img_url = img + '.gif'
+                            if img.endswith('jpg'):
+                                img_url = img + '.jpg'
+                            if img.endswith('jpeg'):
+                                img_url = img + '.jpeg'
+                            if img.endswith('png'):
+                                img_url = img + '.png'
+
+                            # 下载图片到本地
+                            # data = requests.get(img,headers=self.headers)
+                            # fp = open(path+img_url[-15:],'wb')
+                            # fp.write(data.content)
+                            # fp.close()
+                        # print(imgs)
+
+                        # 获取html代码
+                        html = requests.get(article_url)
+                        html.encoding = 'utf-8'
+
+                        # 保存数据到数据库
+                        sql = 'INSERT INTO wechat_article(publish_date,article_title,wechat_id,article_url,cover_img,article_content,article_img,article_html) values(%s, %s, %s, %s, %s, %s, %s, %s)'
+                        # 推文为分享其他文章则不入库
+                        if content != 'shareArticle':
+                            try:
+                                self.cursor.execute(sql,(date, title, wechat_id, article_url, pic, content, imgs[0], html.text))
+                                self.db.commit()
+                                self.log(u'入库成功')
+                            except:
+                                self.db.rollback()
+                                self.log(u'入库不成功')
+
+                        #time.sleep(1)
+
+        self.db.close()
 
     def get_atticle_info(self,url):
         '''
@@ -199,9 +206,7 @@ class Spider:
 if __name__ == '__main__':
 
     ids = ['莞工青年', '占豪', '新华社', '央视新闻']
-    for id in ids:
-        db = pymysql.connect(host='localhost', user='root', password='12345678', port=3306, db='spider')
-        cursor = db.cursor()
-        Spider(id).get_url()
-        db.close()
+
+    Spider(ids).get_infos()
+
     Spider.log("爬虫已完成任务")
